ea4ac266fa946e4388996b7790dcb4d9f2f4ce9a
Vineet Biyani <133802493+vineet-biyani@users.noreply.github.com>
2023-07-03 12:03:48 +0530

    Add files via upload

---
+++ what_is_your_name.py
@@ -0,0 +1,8 @@
+def your_name(first_name, last_name):
+    return f"Hello {first_name} {last_name}! You just delved into python."
+
+
+f_name = input("")
+l_name = input("")
+result = your_name(f_name, l_name)
+print(result)
