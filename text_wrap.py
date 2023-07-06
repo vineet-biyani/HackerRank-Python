13dd298d6b69a0382ba915563854eb2bc68191e6
Vineet Biyani <133802493+vineet-biyani@users.noreply.github.com>
2023-07-06 14:54:36 +0530

    Add files via upload

---
+++ text_wrap.py
@@ -0,0 +1,36 @@
+def wrap(string_to_wrap, max_width):
+    wrapped_list = [string_to_wrap[i:i + max_width] for i in range(0, len(string_to_wrap), max_width)]
+    return '\n'.join(wrapped_list)
+
+
+string = input("")
+width = int(input(""))
+result = wrap(string, width)
+print(result)
+
+
+# ALTERNATE METHOD 1
+import textwrap
+
+
+def wrap(wrapping_string, wrapping_width):
+    return '\n'.join(textwrap.wrap(wrapping_string, wrapping_width))
+
+
+string = input("")
+width = int(input(""))
+result = wrap(string, width)
+print(result)
+
+
+# ALTERNATE METHOD 2
+def wrap(string_for_wrapping, wrap_width):
+    new_string = ''
+    for i in range(0, len(string_for_wrapping), wrap_width):
+        new_string = new_string + string_for_wrapping[i:i+wrap_width] + '\n'
+    return new_string.strip()
+
+
+string, width = input(), int(input())
+result = wrap(string, width)
+print(result)
